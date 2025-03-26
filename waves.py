3984f97c2f1bcb98e3b1c8e8d2aea57877b85f53
Colin Beyers <colin_beyers@mines.edu>
2025-03-25 19:51:19 -0600

    adds redundency for when t is a float/int

--- waves.py
+++ waves.py
@@ -4,6 +4,10 @@ def eta(x, t, a, k, omega):
     """
     Computes waveform at each t across space x.
     """
+
+    if isinstance(t, (int, float)):
+        t = np.array([t])
+    
     eta = a*np.cos(k * x[np.newaxis, :] - omega * t[:, np.newaxis])
 
     return eta
