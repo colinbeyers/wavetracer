34b39c43f170e726b3b109b06cb63b4ab6617668
Colin Beyers (Student) <colin_beyers@mines.edu>
2025-05-06 13:07:22 -0600

    changes naming convention for approximation

--- excursion_vectors/first_order.py
+++ excursion_vectors/first_order.py
@@ -70,8 +70,8 @@ def xi_intermediate(a: float, k: float, H: float, omega: float, t: Union[float, 
     Returns:
         Union[float, np.ndarray]: Horizontal excursion vector (xi) at time(s) t.
     """
-    L1 = math.cosh(k*(z_0+H)) / math.sinh(k*H)
-    L2 = math.sinh(k*(z_0+H)) / math.sinh(k*H)
+    L1 = np.cosh(k*(z_0+H)) / np.sinh(k*H)
+    L2 = np.sinh(k*(z_0+H)) / np.sinh(k*H)
     phi = k*x_0 - omega*t
 
     xi_2_intm = -a*L1*np.sin(phi) + a**2*omega*k/2*(L1**2+L2**2)*t + a**2*k/4*(L2**2-L1**2)*np.sin(2*phi)
@@ -96,7 +96,7 @@ def zeta_intermediate(a: float, k: float, H: float, omega: float, t: Union[float
     Returns:
         Union[float, np.ndarray]: Vertical excursion vector (zeta) at time(s) t.
     """
-    L2 = math.sinh(k*(z_0+H)) / math.sinh(k*H)
+    L2 = np.sinh(k*(z_0+H)) / np.sinh(k*H)
     phi = k*x_0 - omega*t
 
     zeta_2_intm = a*L2*np.cos(phi)
@@ -121,7 +121,7 @@ def xi_deep(a: float, k: float, H: float, omega: float, t: Union[float, np.ndarr
     Returns:
         Union[float, np.ndarray]: Horizontal excursion vector (xi) at time(s) t.
     """
-    L1 = math.exp(k*z_0)
+    L1 = np.exp(k*z_0)
     phi = k*x_0 - omega*t
 
     xi_2_deep = -a*L1*np.sin(phi) + a**2*omega*k*L1**2*t
@@ -146,7 +146,7 @@ def zeta_deep(a: float, k: float, H: float, omega: float, t: Union[float, np.nda
     Returns:
         Union[float, np.ndarray]: Vertical excursion vector (zeta) at time(s) t.
     """
-    L2 = math.exp(k*z_0)
+    L2 = np.exp(k*z_0)
     phi = k*x_0 - omega*t
 
     zeta_2_deep = a*L2*np.cos(phi)
