36bfddb6ba71cc46662311a189ff7a8e6192c539
Colin Beyers (Student) <colin_beyers@mines.edu>
2025-05-06 13:06:14 -0600

    adds function to compute CFL

--- utils.py
+++ utils.py
@@ -1,20 +1,23 @@
 import math
-import numpy as np
 
 g = 9.81 # m/s
 
 def k_from_lam(lam):
-    return 2 * np.pi / lam
+    return 2 * math.pi / lam
 
 def lam_from_k(k):
-    return 2 * np.pi / k
+    return 2 * math.pi / k
 
 def wave_disp_relation(k, H, wave_regime):
     if wave_regime is "shallow":
         omega = math.sqrt(g*k)
     elif wave_regime is "intermediate":
-        omega = math.sqrt(g*k*np.tanh(k*H))
+        omega = math.sqrt(g*k*math.tanh(k*H))
     else:
         omega = k*math.sqrt(g*H)
 
     return omega
+
+def cfl(omega):
+    T = 2 * math.pi / omega
+    return T / 24
