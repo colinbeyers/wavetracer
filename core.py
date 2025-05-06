958d9f8d85b1b719840996dce2295a2e82d11732
Colin Beyers (Student) <colin_beyers@mines.edu>
2025-05-06 13:05:07 -0600

    adds functionality for direct free surface calculation

--- core.py
+++ core.py
@@ -3,58 +3,55 @@ import numpy as np
 import xarray as xr
 
 from .parameters import validate_params, categorize_wave_regime, k_from_lam, lam_from_k, wave_disp_relation
-from .waves import eta
-from wavetracer.excursion_vectors.first_order_approx import (
-    xi_shallow          as xi_shallow_first, 
-    zeta_shallow        as zeta_shallow_first, 
-    xi_intermediate     as xi_intermediate_first, 
-    zeta_intermediate   as zeta_intermediate_first, 
-    xi_deep             as xi_deep_first, 
-    zeta_deep           as zeta_deep_first
+from . import utils
+
+from excursion_vectors.leading_order import (
+    xi_shallow          as xi_shallow_0, 
+    zeta_shallow        as zeta_shallow_0, 
+    xi_intermediate     as xi_intermediate_0, 
+    zeta_intermediate   as zeta_intermediate_0, 
+    xi_deep             as xi_deep_0, 
+    zeta_deep           as zeta_deep_0
 )
 
-from wavetracer.excursion_vectors.second_order_approx import (
-    xi_shallow          as xi_shallow_second, 
-    zeta_shallow        as zeta_shallow_second, 
-    xi_intermediate     as xi_intermediate_second, 
-    zeta_intermediate   as zeta_intermediate_second, 
-    xi_deep             as xi_deep_second, 
-    zeta_deep           as zeta_deep_second
+from excursion_vectors.first_order import (
+    xi_shallow          as xi_shallow_1, 
+    zeta_shallow        as zeta_shallow_1, 
+    xi_intermediate     as xi_intermediate_1, 
+    zeta_intermediate   as zeta_intermediate_1, 
+    xi_deep             as xi_deep_1, 
+    zeta_deep           as zeta_deep_1
 )
 
 # Define the function map globally at the module level
 function_map = {
-    "shallow": {
-        "first": (xi_shallow_first, zeta_shallow_first),
-        "second": (xi_shallow_second, zeta_shallow_second),
+    'shallow': {
+        'leading': (xi_shallow_0, zeta_shallow_0),
+        'first': (xi_shallow_1, zeta_shallow_1),
     },
-    "intermediate": {
-        "first": (xi_intermediate_first, zeta_intermediate_first),
-        "second": (xi_intermediate_second, zeta_intermediate_second),
+    'intermediate': {
+        'leading': (xi_intermediate_0, zeta_intermediate_0),
+        'first': (xi_intermediate_1, zeta_intermediate_1),
     },
-    "deep": {
-        "first": (xi_deep_first, zeta_deep_first),
-        "second": (xi_deep_second, zeta_deep_second),
+    'deep': {
+        'leading': (xi_deep_0, zeta_deep_0),
+        'first': (xi_deep_1, zeta_deep_1),
     },
 }
 
-def random_mean_pos(N, x_start, x_end, H):
+def random_mean_positions(N, x_start, x_end, h):
     x_0 = np.random.uniform(x_start, x_end, N)
-    z_0 = np.random.uniform(-H, 0, N)
+    z_0 = np.random.uniform(-h, 0, N)
     return x_0, z_0
 
-import numpy as np
-import xarray as xr
-
-def build_dataset(x_0=None, z_0=None, time=None, grid=False, random_pos=False, N=None, x_min=None, x_max=None, H_particles=None):
+def build_dataset(x0=None, z0=None, random_pos=False, N=None, x_min=None, x_max=None, H_particles=None, **kwargs):
     """
-    Builds an xarray Dataset with particle trajectories.
+    Builds an xarray Dataset with mean particle positions and wave parameters.
 
     Parameters:
     - x_0: Initial x positions of particles (array-like, required if random_pos=False).
     - z_0: Initial z positions of particles (array-like, required if random_pos=False).
     - time: Time array.
-    - grid: If True, create a meshgrid for x_0 and z_0. If False, treat x_0 and z_0 as 1D arrays.
     - random_pos: If True, generate random positions instead of using x_0, z_0.
     - N: Number of random particles (required if random_pos=True).
     - x_min: Start of x range for random positions (required if random_pos=True).
@@ -64,111 +61,133 @@ def build_dataset(x_0=None, z_0=None, time=None, grid=False, random_pos=False, N
     Returns:
     - ds: xarray Dataset containing the particle data.
     """
-    
     # If random_pos is True, generate random positions
     if random_pos:
         if None in [N, x_min, x_max, H_particles]:
             raise ValueError("When random_pos=True, N, x_min, x_max, and H_particles must be provided.")
-        # Generate random positions
-        x_rand, z_rand = random_mean_pos(N, x_min, x_max, H_particles)
+        x_rand, z_rand = random_mean_positions(N, x_min, x_max, H_particles)
         
         # If x_0 and z_0 are provided, concatenate them with the random positions
-        if x_0 is not None and z_0 is not None:
-            if len(x_0) != len(z_0):
+        if x0 is not None and z0 is not None:
+            if len(x0) != len(z0):
                 raise ValueError("x_0 and z_0 must have the same length.")
-            # Concatenate the provided x_0, z_0 with the random values
-            x_0 = np.concatenate([x_0, x_rand])
-            z_0 = np.concatenate([z_0, z_rand])
+            x0 = np.concatenate([x0, x_rand])
+            z0 = np.concatenate([z0, z_rand])
         else:
-            # Only use the random positions
-            x_0, z_0 = x_rand, z_rand
+            x0, z0 = x_rand, z_rand
 
     else:
         # If random_pos is False, x_0 and z_0 must be provided
-        if x_0 is None or z_0 is None:
+        if x0 is None or z0 is None:
             raise ValueError("When random_pos=False, x_0 and z_0 must be provided.")
         
         # Validate that x_0 and z_0 have the same length
-        if len(x_0) != len(z_0):
-            raise ValueError("x_0 and z_0 must have the same length when grid=False.")
-        
-        # If grid is True, create meshgrid and flatten
-        if grid:
-            x_0, z_0 = np.meshgrid(x_0, z_0)
-            x_0 = x_0.flatten()
-            z_0 = z_0.flatten()
+        if len(x0) != len(z0):
+            raise ValueError("x0 and z0 must have the same length when grid=False.")
 
     # Build the xarray Dataset
     ds = xr.Dataset(
         {
-            "x_0": ("particle", x_0),
-            "z_0": ("particle", z_0),
+            "x0": ("particle", x0),
+            "z0": ("particle", z0),
         },
         coords={
-            "time": time,
-            "particle": np.arange(len(x_0)),
+            "particle": np.arange(len(x0)),
         },
     )
-    
+
     return ds
 
-def compute_trajectories(ds, **kwargs):
-    # Extract parameters from kwargs with default values
-    a           = kwargs.get('a')
-    H           = kwargs.get('H')
-    k           = kwargs.get('k')
-    lam         = kwargs.get('lam', None)
-    order       = kwargs.get('order', "first")  # Default to "first" if not provided
+def compute_trajectories(ds, duration, **kwargs):
+    # Extract physical parameters from kwargs
+    a       = kwargs.get('a', None)
+    H       = kwargs.get('H', None)
+    k       = kwargs.get('k', None)
+    lam     = kwargs.get('lam', None)
+    order   = kwargs.get('order', 'leading')
 
     validate_params(a=a, H=H, k=k, lam=lam, order=order)
-    
-    # Calculate wavenumber if wavelength is provided (or vice versa)
-    if lam is not None and k is None:
-        k = k_from_lam(lam)  # k from lam
-    elif k is not None and lam is None:
-        lam = lam_from_k(k)  # lam from k
-    
-    # Determine the wave regime based on the water depth and wavenumber
-    wave_regime = categorize_wave_regime(k, H)
-    # print("Regime: ", wave_regime)
 
-    omega = wave_disp_relation(k=k, H=H, wave_regime=wave_regime)
+    # Calculate k if lam is provided (or vice versa)
+    if lam is not None and k is None:
+        k = k_from_lam(lam)
+    elif k is not None and lam is None:
+        lam = lam_from_k(k)
+
+    wave_regime = categorize_wave_regime(k, H)
+    omega       = wave_disp_relation(k=k, H=H, wave_regime=wave_regime)
+
+    # Calculate ideal dt and time array and add to dataset
+    dt      = utils.cfl(omega)
+    time    = np.arange(0, duration, dt)
+    ds      = ds.assign_coords(time=("time", time))
 
     # Create a dictionary of wave parameters to pass as kwargs
-    params = {
-        'a': a,           # Wave amplitude
-        'H': H,           # Water depth
-        'k': k,           # Wavenumber
-        'omega': omega,   # Angular frequency
-        'order': order,   # Approximation order
-        'wave_regime': wave_regime  # Wave regime
+    wave_params = {
+        'a': a,
+        'H': H,         
+        'k': k,        
+        'omega': omega,
+        'order': order,
+        'wave_regime': wave_regime
     }
 
     # Apply the trajectory calculation using kwargs
-    ds['x_p'], ds['z_p'] = xr.apply_ufunc(
+    ds['xp'], ds['zp'] = xr.apply_ufunc(
         compute_trajectory,
-        ds['x_0'], ds['z_0'], ds['time'],
-        kwargs=params,  # Pass the parameters as a dictionary of keyword arguments
+        ds['x0'], ds['z0'], ds['time'],
+        kwargs=wave_params,  # Pass the parameters as a dictionary of keyword arguments
         input_core_dims=[[], [], ['time']],
         output_core_dims=[['time'], ['time']],
         vectorize=True,  # Apply function across particles and time
         dask='allowed'  # Optional: Enable parallelism with Dask
     )
 
-    if order == 'second':
-        min = int(ds.x_p.min())-1
-        max = int(ds.x_p.max())+1
-    else:
-        min = int(ds.x_0.min())-5
-        max = int(ds.x_0.max())+5
+    ds.attrs.update({
+        'a': {'value': a, 'description': 'Wave amplitude (meters)'},
+        'H': {'value': H, 'description': 'Water depth (meters)'},
+        'k': {'value': k, 'description': 'Wavenumber (1/meters)'},
+        'lam': {'value': lam, 'description': 'Wavelength (meters)'},
+        'order': {'value': order, 'description': 'Approximation order ("leading" or "first")'}
+    })
 
-    x_eta = np.linspace(min, max, int((max-min)/lam * 32))
-    # print(x_eta)
-    ds.coords['x_eta'] = x_eta  # Add 'x' as a coordinate to the dataset
+    ds_eta = compute_free_surface(ds, wave_params)
 
-    ds['eta'] = (('time', 'x_eta'), eta(x_eta, ds['time'].values, a, k, omega))
+    x_eta_array = ds_eta['xp'].transpose('time', 'particle').data
+    z_eta_array = ds_eta['zp'].transpose('time', 'particle').data
+
+    ds = ds.assign(
+        x_eta=(('time', 'point'), x_eta_array),
+        z_eta=(('time', 'point'), z_eta_array)
+    )
 
     return ds
+
+def compute_free_surface(ds_p, wave_params):
+    lam     = ds_p.attrs['lam']['value']
+    time    = ds_p['time']
+
+    x_min = int(ds_p.xp.min()) - 0.1 * lam
+    x_max = int(ds_p.xp.max()) + 0.1 * lam
+
+    x0_eta = np.linspace(x_min, x_max, int((x_max - x_min) / lam * 32))
+    z0_eta = np.zeros_like(x0_eta)
+
+    ds_eta = build_dataset(x0=x0_eta, z0=z0_eta, random_pos=False)
+    ds_eta = ds_eta.assign_coords(time=("time", time.values))
+
+    # calculate free surface particle trajectories
+    ds_eta['xp'], ds_eta['zp'] = xr.apply_ufunc(
+        compute_trajectory,
+        ds_eta['x0'], ds_eta['z0'], ds_eta['time'],
+        kwargs=wave_params,
+        input_core_dims=[[], [], ['time']],
+        output_core_dims=[['time'], ['time']],
+        vectorize=True,
+        dask='allowed'
+    )
+
+    return ds_eta
 
 def compute_trajectory(x_0, z_0, t, **kwargs):
     """
